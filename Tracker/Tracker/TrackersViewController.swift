65d0ad599223ae1c3382c2da81445d228d85967e
Ann <bkljn4g@gmail.com>
2023-07-08 16:01:19 +0300

    Добавлены иконки по проекту

---
+++ Tracker/Tracker/TrackersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TrackersViewController.swift
+//  Tracker
+//
+//  Created by Ann Goncharova on 07.07.2023.
+//
+
+import UIKit
+
+final class TrackersViewController: UIViewController {
+    private let mainTitle = UILabel()
+    private let currentDate: Int?
+    private let searchField = UISearchTextField()
+    private let stubStar = UIImage()
+    private let
+}()
+
+
