c72af735769fbe418b8d9929083ae32cc6ea1f78
Anna Goncharova <bkljn4g@gmail.com>
2023-10-23 17:04:08 +0300

    фильтр категорий

--- Tracker/Tracker/Struct/TrackerCategory.swift
+++ Tracker/Tracker/Struct/TrackerCategory.swift
@@ -11,4 +11,12 @@ struct TrackerCategory {
     
     let name: String
     let trackers: [Tracker]
+    
+    func visibleTrackers(filterString: String) -> [Tracker] {
+        if filterString.isEmpty {
+            return trackers
+        } else {
+            return trackers.filter { $0.name.lowercased().contains(filterString.lowercased()) }
+        }
+    }
 }
