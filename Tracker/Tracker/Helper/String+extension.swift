a797951ee10b5636f9605c7727eca8cc9cb9f311
Anna Goncharova <bkljn4g@gmail.com>
2023-10-23 16:15:57 +0300

    добавлен экстеншн для конвертации строки с цветом в UIColor

---
+++ Tracker/Tracker/Helper/String+extension.swift
@@ -0,0 +1,23 @@
+//
+//  String+extension.swift
+//  Tracker
+//
+//  Created by Anka on 22.10.2023.
+//
+
+import UIKit
+
+// конвертация строки с цветом в объект юайКолор
+
+extension String {
+    var color: UIColor {
+        var rgbValue:UInt64 = 0
+        Scanner(string: self).scanHexInt64(&rgbValue)
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
