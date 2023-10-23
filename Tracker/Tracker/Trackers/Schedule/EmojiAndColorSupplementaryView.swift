2a2267b4abaa249bc531aa73fdd249be22079f03
Anna Goncharova <bkljn4g@gmail.com>
2023-10-23 14:15:27 +0300

    добавлен скролвью с выбором эмодзи и цвета

---
+++ Tracker/Tracker/Trackers/Schedule/EmojiAndColorSupplementaryView.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiAndColorSupplementaryView.swift
+//  Tracker
+//
+//  Created by Ann Goncharova on 21.10.2023.
+//
+
+import UIKit
+
+final class EmojiAndColorSupplementaryView: UICollectionReusableView {
+    
+    static var identifier = "header"
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 19)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
