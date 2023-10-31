5efb0286d21d13ea3a8816735e998c750f0da0e4
Anna Goncharova <bkljn4g@gmail.com>
2023-10-31 03:14:53 +0300

    мелкие правки

--- Tracker/Tracker/Trackers/RegularOrIrregularEventVC/RegularOrIrregularEventVC.swift
+++ Tracker/Tracker/Trackers/RegularOrIrregularEventVC/RegularOrIrregularEventVC.swift
@@ -14,14 +14,14 @@ protocol RegularOrIrregularEventVCDelegate: AnyObject {
 }
 
 class RegularOrIrregularEventVC: UIViewController {
-    
+    private let colors = Colors()
     public weak var delegate: RegularOrIrregularEventVCDelegate?
     
     private lazy var label: UILabel = {
         let label = UILabel()
-        label.textColor = .black
+        label.textColor = .ypBlack
         label.text = "Создание трекера"
-        label.font = .systemFont(ofSize: 16, weight: .medium) // поменяла на .medium
+        label.font = .systemFont(ofSize: 16, weight: .medium)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -29,7 +29,8 @@ class RegularOrIrregularEventVC: UIViewController {
     private lazy var createRegularEventButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setTitle("Привычка", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium) // поправила шрифт кнопки
+        button.setTitleColor(.ypWhite, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         button.backgroundColor = .ypBlack
         button.layer.cornerRadius = 16
         button.addTarget(self, action: #selector(regularEventButtonAction), for: .touchUpInside)
@@ -40,7 +41,8 @@ class RegularOrIrregularEventVC: UIViewController {
     private lazy var createIrregularEventButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setTitle("Нерегулярное событие", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium) // поправила шрифт кнопки
+        button.setTitleColor(.ypWhite, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         button.backgroundColor = .ypBlack
         button.layer.cornerRadius = 16
         button.addTarget(self, action: #selector(irregularEventButtonAction), for: .touchUpInside)
@@ -50,7 +52,7 @@ class RegularOrIrregularEventVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = colors.viewBackgroundColor
         addSubviews()
         setupLayout()
     }
